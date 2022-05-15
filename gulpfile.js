b84f2d16f2ae53e99176ed6ca630dae537385555
Paschal Onuorah <onuorahpaschal@gmail.com>
2022-05-15 16:20:57 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require("gulp");
+const gap = require("gulp-append-prepend");
+
+gulp.task("licenses", async function () {
+  gulp
+    .src("build/static/js/*chunk.js", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+      =========================================================
+      * E-Top Transaction Monitoring Dashboard - v1.0.0
+      =========================================================
+      
+      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      */`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production mode for the minified html
+  gulp
+    .src("build/index.html", { base: "./" })
+    .pipe(
+      gap.prependText(`<!--
+      /*!
+      
+      =========================================================
+      * E-Top Transaction Monitoring Dashboard - v1.0.0
+      =========================================================
+      
+      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      */
+      -->`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production mode for the minified css
+  gulp
+    .src("build/static/css/*chunk.css", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+      =========================================================
+      * E-Top Transaction Monitoring Dashboard - v1.0.0
+      =========================================================
+      
+      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      */`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  return;
+});
---
+++ src/routes.js
@@ -0,0 +1,80 @@
+// import
+import Dashboard from "views/Dashboard/Dashboard.js";
+import Tables from "views/Dashboard/Tables.js";
+import Billing from "views/Dashboard/Billing.js";
+import RTLPage from "views/RTL/RTLPage.js";
+import Profile from "views/Dashboard/Profile.js";
+import SignIn from "views/Pages/SignIn.js";
+import SignUp from "views/Pages/SignUp.js";
+
+import {
+  HomeIcon,
+  StatsIcon,
+  CreditIcon,
+  PersonIcon,
+  DocumentIcon,
+  RocketIcon,
+  SupportIcon,
+} from "components/Icons/Icons";
+
+var dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: <HomeIcon color="inherit" />,
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    icon: <StatsIcon color="inherit" />,
+    component: Tables,
+    layout: "/admin",
+  },
+  {
+    path: "/billing",
+    name: "Billing",
+    icon: <CreditIcon color="inherit" />,
+    component: Billing,
+    layout: "/admin",
+  },
+  {
+    path: "/rtl-support-page",
+    name: "RTL",
+    icon: <SupportIcon color="inherit" />,
+    component: RTLPage,
+    layout: "/rtl",
+  },
+  {
+    name: "ACCOUNT PAGES",
+    category: "account",
+    state: "pageCollapse",
+    views: [
+      {
+        path: "/profile",
+        name: "Profile",
+        icon: <PersonIcon color="inherit" />,
+        secondaryNavbar: true,
+        component: Profile,
+        layout: "/admin",
+      },
+      {
+        path: "/signin",
+        name: "Sign In",
+        icon: <DocumentIcon color="inherit" />,
+        component: SignIn,
+        layout: "/auth",
+      },
+      {
+        path: "/signup",
+        name: "Sign Up",
+        icon: <RocketIcon color="inherit" />,
+        secondaryNavbar: true,
+        component: SignUp,
+        layout: "/auth",
+      },
+    ],
+  },
+];
+export default dashRoutes;
